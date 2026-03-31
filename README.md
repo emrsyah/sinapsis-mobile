485e982b3576deae280e3b1029d3cef7e4d22c44
Emirsyah <muhammademir48@gmail.com>
2026-03-31 18:23:31 +0700

    feat(init): scaffold project

---
+++ README.md
@@ -0,0 +1,3 @@
+# sinapsis_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/sinapsis_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sinapsis_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
